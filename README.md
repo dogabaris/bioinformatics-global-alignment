be2e3eb00841ae01018abc3a6c6e18ba1cdd9fa7
dogabaris <dogabarisozdemir@gmail.com>
2016-10-08 15:01:51 +0300

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# biyoinformatik-odev1
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string dna1,dna2;
+  std::cout << "1.dna :";std::cin >> dna1;
+  std::cout << "2.dna :"; std::cin >> dna2;
+
+  std::cout << dna1<< " " << dna2 << std::endl;
+  return 0;
+}
