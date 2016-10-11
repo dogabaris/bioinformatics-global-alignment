79d5aa8aa2230c4af58bd2a5d12c6e5e5837aeb2
dogabaris <dogabarisozdemir@gmail.com>
2016-10-11 23:04:22 +0300

    minor çizdirme

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 //tablo matrisi
 //tablo doldurulurken yollar matrisine olası yollar eklenecek
 //sağ en alttan başlanıp olası yollara göre high road low road ve
+//diagonal giderken(çapraz) match-mismatch eklenir, sağdan ya da soldan giderken gap penalty eklenir. En büyük değer yazılır.
 
 int StringToInt( const std::string& st)
 {
@@ -67,6 +68,7 @@ int main() {
       yonler[i].resize(dna1.size()+1);
 
   matrix[1][1]=0;
+
   for(int i=2;i<dna1.size()+2;i++){//satır - genişlik
       matrix[0][i]=dna1[i-2];
       matrix[1][i]=matrix[1][i-1]+gap;
@@ -75,16 +77,39 @@ int main() {
       matrix[k][0]=dna2[k-2];
       matrix[k][1]=matrix[k-1][1]+gap;
   }
+//diagonal gel satir ve sütundan 1 er eksik.üstten columdan 1 eksik.yandan rowdan 1 eksik.
+  int tmpdiag,tmpup,tmpright;
+  for(int r=2;r<dna2.size();r++){//row ,2
+    for(int c=2;c<dna1.size();c++){//column
+      if(matrix[0][c]==matrix[r][0])
+        tmpdiag=match+matrix[r-1][c-1];
+
+      else
+        tmpdiag=mismatch+matrix[r-1][c-1];
+
+        tmpup=gap+matrix[r-1][c];
+        tmpright=gap+matrix[r][c-1];
+
+        int biggest = tmpdiag;
+
+        if (tmpdiag <= tmpup)
+              biggest = tmpup;
+        if (biggest <= tmpright)
+              biggest = tmpright;
+
+        matrix[r][c]=biggest;
+    }
+  }
 
   for(int i = 0; i < dna2.size()+2; i++){//matrisi çizdiriyor
         for(int j = 0; j < dna1.size()+2; j++){
-          //if(i==0 && j>=2)
+          //if(i==1 && j=1)
           //  cout << matrix[i][j] << " ";
           //else
             cout << matrix[i][j] << " ";
         }
         cout << endl;
-    }
+  }
 
   return 0;
 }
